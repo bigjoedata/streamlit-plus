08192ebe335cf1dc7744ddabef3eb2a05d49805c
jb <blechman@gmail.com>
2021-01-07 15:56:45 -0500

    Slight upgrades

--- dep_upgrade_install.sh
+++ dep_upgrade_install.sh
@@ -1,4 +1,4 @@
 pip install --upgrade streamlit pandas scikit-learn numpy bokeh altair matplotlib plotly seaborn gspread quandl streamlit-pandas-profiling streamlit-embedcode streamlit-bokeh-events st-annotated-text oauth2client oauthlib plotnine
 pip freeze > requirements.txt
 pip install --upgrade pystan
-pip install --upgrade pycaret hiplot streamlit-vega-lite streamlit-observable spacy-streamlit streamlit-folium fbprophet
+pip install --upgrade pycaret hiplot streamlit-vega-lite streamlit-observable spacy-streamlit streamlit-folium fbprophet texthero beautifulsoup4
--- dockerbuild.sh
+++ dockerbuild.sh
@@ -3,4 +3,4 @@ echo Building lighter image
 docker build -f Dockerfile -t intelligentdesigns/streamlit-plus:latest .
 
 echo Build heavier image
-docker build -f Dockerfile_extra -t intelligentdesigns/streamlit-plus-extra:latest .
+docker build -f Dockerfile_extra -t intelligentdesigns/streamlit-plus:extra-latest .
